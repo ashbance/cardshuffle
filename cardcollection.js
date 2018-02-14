c92234febba0def7e9a61c4b5f99d48bdd5e333e
ashbance <ashok.bance@gmail.com>
2018-02-14 20:08:46 +0000

    updated the comment

--- cardcollection.js
+++ cardcollection.js
@@ -4,8 +4,8 @@ var NUM_CARDS = 13;
 var cards = [];
 
 function buildDeck() {
-
-
+    
+    //add a comment again
     for (int i = 0; i < NUM_SUITS; i++) {
         for (int j = 1; j <= NUM_CARDS; j++) {
             cards.push(i + " " j);
